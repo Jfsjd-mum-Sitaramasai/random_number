1ba00ecd6bca80e653567accec49bebe0f4b89fb
sitaramasai <jfsjd.mum.sitaramasai7@gmail.com>
2022-10-31 11:56:50 +0530

    file is added

---
+++ random_number/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String args[]) {
+        Random rand = new Random();
+
+            int rand_int1 = rand.nextInt(1, 10);
+
+
+        System.out.println("Random Integers: " + rand_int1);
+    }
+}
